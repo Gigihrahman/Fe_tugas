7fc5817faedc0b637ccfa9bfd66143f5d0c8c23d
Gigihrahman <gigihrahmanditta@gmail.com>
2024-08-05 21:03:49 +0700

    yoklah

--- src/components/Elements/footer/footer.jsx
+++ src/components/Elements/footer/footer.jsx
@@ -12,9 +12,9 @@ export const Footer = () => {
             class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
             <img
-              src="../../../../public/images/logo.png"
+              src="/images/logo.png"
               class="h-8"
-              alt="Flowbite Logo"
+              alt="ngado kado"
             />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Ngado Kado
